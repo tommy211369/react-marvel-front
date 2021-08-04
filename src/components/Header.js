242e51d096a57157a3bae5240d3469b09af4edf8
tommy211369 <tommy.thongrasamy@gmail.com>
2021-08-04 16:59:31 +0200

    add avengers fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,23 @@
 import React from "react";
+import logo from "../assets/img/logo.jpeg";
+import { Link } from "react-router-dom";
+//logo marvel
 // navigation Chracters, Comics, Favorites
 
 export default function Header() {
-  return <div>Header Component</div>;
+  return (
+    <div className="header">
+      <div className="header-img">
+        <img src={logo} alt="marvel" />
+      </div>
+
+      <nav>
+        <ul>
+          <Link to="/">Personnages</Link>
+          <Link to="/comics">Comics</Link>
+          <Link to="/favorites">Favoris</Link>
+        </ul>
+      </nav>
+    </div>
+  );
 }
