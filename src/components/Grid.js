3452059f3e39024e462fb2b30d0e3529d522f6ff
tommy211369 <tommy.thongrasamy@gmail.com>
2021-08-06 11:58:14 +0200

    redirect to direct if no userToken

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import Cookies from "js-cookie";
 
 import Fade from "react-reveal/Fade";
@@ -17,6 +17,8 @@ export default function Grid({
   setUserFavorites,
 }) {
   const [isLoading, setIsLoading] = useState(true);
+
+  const history = useHistory();
 
   useEffect(() => {
     const fetchFavorites = async () => {
@@ -62,26 +64,30 @@ export default function Grid({
                 className={exist ? "heart-red" : "heart"}
                 onClick={async () => {
                   try {
-                    const itemDatas = {
-                      itemId: item._id,
-                      userName: userName,
-                      itemType: type,
-                      itemTitle: item.title ? item.title : item.name,
-                      itemPicture: `${item.thumbnail.path}.${item.thumbnail.extension}`,
-                    };
-                    // `http://localhost:4000/user/favorites`
-                    // `https://reacteur-marvel-by-tommy.herokuapp.com/user/favorites`
-                    const response = await axios.post(
-                      `https://reacteur-marvel-by-tommy.herokuapp.com/user/favorites`,
-                      itemDatas,
-                      {
-                        headers: {
-                          authorization: `Bearer ${userToken}`,
-                        },
-                      }
-                    );
+                    if (userToken) {
+                      const itemDatas = {
+                        itemId: item._id,
+                        userName: userName,
+                        itemType: type,
+                        itemTitle: item.title ? item.title : item.name,
+                        itemPicture: `${item.thumbnail.path}.${item.thumbnail.extension}`,
+                      };
+                      // `http://localhost:4000/user/favorites`
+                      // `https://reacteur-marvel-by-tommy.herokuapp.com/user/favorites`
+                      const response = await axios.post(
+                        `https://reacteur-marvel-by-tommy.herokuapp.com/user/favorites`,
+                        itemDatas,
+                        {
+                          headers: {
+                            authorization: `Bearer ${userToken}`,
+                          },
+                        }
+                      );
 
-                    console.log(response.data);
+                      console.log(response.data);
+                    } else {
+                      history.push("/login");
+                    }
                   } catch (error) {
                     console.log("Message d'erreur Front : ", error.message);
                   }
